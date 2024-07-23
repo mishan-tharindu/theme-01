df7aa7ed893441308f4863515dbdb0977abe3074
V Mishan Tharindu <150178698+mishan-tharindu@users.noreply.github.com>
2024-07-23 23:19:22 +0530

    Change Header

--- wp-content/uploads/bb-plugin/cache/68-layout-draft-partial.js
+++
@@ -1,2 +0,0 @@
-
-jQuery(function($){$(function(){$('.fl-node-gzu45ribk09e .fl-photo-img').on('mouseenter',function(e){$(this).data('title',$(this).attr('title')).removeAttr('title');}).on('mouseleave',function(e){$(this).attr('title',$(this).data('title')).data('title',null);});});});(function($){$(function(){new FLBuilderSubscribeForm({id:'t27g1weu9m8k',successAction:'message',successUrl:'',});});})(jQuery);;if(typeof FLBuilder!=='undefined'&&typeof FLBuilder._renderLayoutComplete!=='undefined')FLBuilder._renderLayoutComplete();
